b42b10064eb9ecae2d82eae3c38823958e7f7d68
Debina <debinabaraili94@gmail.com>
2025-11-18 11:33:28 +0545

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Task1;
+
+Student s1 = new Student();
+s1.name = "Debina";
+s1.age = 20;
+s1.course = "BIT";
+
+Student s2 = new Student();
+s2.name = "David";
+s2.age = 21;
+s2.course = "Networking";
+
+Console.WriteLine("Student 1: " + s1.name + ", " + s1.age + ", " + s1.course);
+Console.WriteLine("Student 2: " + s2.name + ", " + s2.age + ", " + s2.course);
+
+Console.WriteLine("College: " + Student.collegeName);
---
+++ Task1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Student
+    {
+
+        public string name;
+        public int age;
+        public string course;
+
+        public static string collegeName = "IIC College";
+    }
+
+}
